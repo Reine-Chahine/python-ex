63402bcdf977135062fdb5ae6579b50d4746af80
Reine-Chahine <reinechahine3@gmail.com>
2025-02-13 23:01:33 +0200

    coding the username and password

---
+++ ex3.py
@@ -0,0 +1,8 @@
+user = input("Enter a username:")
+password = input ("Enter a password:")
+
+if user == "admin" and password == "1234":
+    print ("Access granted")
+
+else:
+    print ("access denied")    
